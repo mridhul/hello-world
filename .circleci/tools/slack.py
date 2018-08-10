ce5456b4c76ce2f82640b9e34df153ce96d5fcd9
mridhul <mridhul@live.com>
2018-08-10 10:56:39 +0530

    Update slack.py

--- .circleci/tools/slack.py
+++ .circleci/tools/slack.py
@@ -8,17 +8,15 @@ attachment = sys.argv[1]
 def slack_message(message, channel):
   token = 'xoxb-412432026433-414214512930-t0y3tau1n0IrMLTmhE43Qtjx'
   sc = SlackClient(token)
-  filename='~/repo/reports/'+attachment
+  filename='/home/circleci/repo/reports/'+attachment
   with open(filename, 'rb') as f:
    sc.api_call(
           "files.upload",
           channels=channel,
-          filename='~/repo/reports/container-scanning-report-2018-08-10-04-19-19.json',
+          filename=filename,
           title='scanout',
           initial_comment='vuln scanning',
           file=io.BytesIO(f.read())
       )
-      
-   
-                
+                      
 slack_message("Hey","devopsifly")
