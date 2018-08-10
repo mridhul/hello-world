09a70b9acf96be8f62104affae58aa8c1340a9a7
mridhul <mridhul@live.com>
2018-08-10 12:02:54 +0530

    Update slack.py

--- .circleci/tools/slack.py
+++ .circleci/tools/slack.py
@@ -10,6 +10,15 @@ def slack_message(message, channel):
   token = 'xoxb-412432026433-414214512930-t0y3tau1n0IrMLTmhE43Qtjx'
   sc = SlackClient(token)
   reportFile='/home/circleci/repo/reports/'+attachment
-  sc.api_call("files.upload", filename=reportFile, channels=channel, file= io.BytesIO(str.encode(content)))
+  #sc.api_call("files.upload", filename=reportFile, channels=channel, file= io.BytesIO(str.encode(content)))
+  with open(reportFile, 'rb') as f:
+    sc.api_call(
+        "files.upload",
+        channels=channel,
+        filename=reportFile,
+        title='sampletitle',
+        initial_comment='sampletext',
+        file=io.BytesIO(f.read())
+    )
                       
 slack_message("Hey","devopsifly")
